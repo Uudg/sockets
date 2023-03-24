d89c89a6de1ac4e9aa1b33e8a82e340c402cbc3b
Danil <danilpankrat@gmail.com>
2023-03-24 03:52:52 +0200

    negr na svnie
    Asyl for you

---
+++ check/main.py
@@ -0,0 +1,7 @@
+from fuck_words import profanity
+
+def check(text):
+    if __name__ == "__main__":
+        censored_text = profanity.censor(text)
+        return censored_text
+    # print("\nI have checked your inputs so here is my evaluation: ", censored_text)
--- server.js
+++ server.js
@@ -4,11 +4,11 @@ const socketIO = require('socket.io');
 const Message = require('./messages');
 const connectDB = require('./db');
 const getAll = require('./get');
-
+cors = require('cors')
 const app = express();
 const server = http.createServer(app);
 const io = socketIO(server);
-
+app.use(cors())
 connectDB();
 
 server.listen(3000, () => {
