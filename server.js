c614837e12ed701887c6203963e5d49db927d46c
Danil <danilpankrat@gmail.com>
2023-03-24 03:59:07 +0200

    ya krutoi

--- server.js
+++ server.js
@@ -10,10 +10,20 @@ const server = http.createServer(app);
 const io = socketIO(server);
 app.use(cors())
 connectDB();
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    next();
+})
 
 server.listen(3000, () => {
     console.log('Server started on port 3000');
 });
+
+app.get('/asyl', async (req, res) => {
+    res.send({
+        msg: 'loh'
+    })
+})
 
 
 app.get('/', (req, res) => {
